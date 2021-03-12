343d4d65cac3ff93b2a181ae4996ff6cd30dc7f8
R Dinuwan Randunu <it18133578@my.sliit.lk>
2021-03-13 00:45:12 +0530

    Add files via upload

---
+++ ctf/level05.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Cyber Heist CTF</title>
+
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="/ctf/css/bootstrap4-neon-glow.min.css">
+
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel='stylesheet' href='//cdn.jsdelivr.net/font-hack/2.020/css/hack.min.css'>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
+    <link rel="stylesheet" href="/ctf/css/main.css">
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
+</head>
+
+    <body>
+    <div class="glitch">
+        <div style="position: fixed;" class="img"></div>
+    </div>
+    
+    <div class="navbar-dark text-white">
+        <div class="container">
+            <nav class="navbar px-0 py-0 navbar-expand-lg navbar-dark">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a href="/ctf/index.php" class="pl-md-0 p-3 text-decoration-none text-light">
+                            <h3 class="bold"><span class="color_danger">CYBER_HEIST_CTF</span><!--<span class="color_white">CTF</span>--></h3>
+                        </a>
+                    </div>
+                    
+                </div>
+            </nav>
+        </div>
+        </div>
+        <div class="jumbotron bg-transparent mb-0 pt-0 radius-0">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12  text-center l1hint" >
+                    <h1 class="display-1 bold color_white content__title">LEVEL<span class="color_danger">05</h1><br>
+                    <p class="color_yellow text-spacey hackerFont lead mb-5">
+                        Hint: <br>Human readable <br>strings <br>with several '+' <br>characters <br>...
+                    </p>
+                    <p class="text-spacey hackerFont lead mb-5">
+                        <a href="./level/level05/bitcoin_data.txt" class="color_white"><br>Find now</a> 
+                        
+                    </p>
+                </div>
+            </div>
+            
+        <nav class="navbar px-0 navbar-expand-lg navbar-dark">
+                    
+        <div class="navbar-nav ml-auto">
+            <a href="/ctf/index.php" class="p-3 text-decoration-none text-lightcol bold bottom-nav">Home</a>
+            <a href="/ctf/about.php" class="p-3 text-decoration-none text-lightcol bold bottom-nav">About</a>
+            <a href="/ctf/leaderboard.php" class="p-3 text-decoration-none text-lightcol bold bottom-nav">Leaderboard</a>
+            
+            <a href="/ctf/challenges.php" class="p-3 text-decoration-none text-white bold">Challenges</a>
+            
+            <a href="/ctf/login.php" class="p-3 text-decoration-none text-lightcol bold">Logout</a>
+        </div>
+                   
+        </nav>
+
+        </div>
+    </div>
+            
+        
+    </body>
+    </html>
